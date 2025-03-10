1869f3f7e8507785ebe3caaf37b2bae13130c491
Vit Vodicka <xvodick6@mendelu.cz>
2025-03-10 21:42:37 +0100

    fixed side rectangles, needs to do split them

--- GridPath/GridPath/Helper/ParcelCalculator.cs
+++ GridPath/GridPath/Helper/ParcelCalculator.cs
@@ -24,18 +24,14 @@ namespace GridPath.Helper
             double EndPointX = coordinates[2].x;
             double EndPointY = coordinates[2].y;
 
-            for (int i = 0; i < 3; i++)
-            {
-                switch (i)
-                {
-                    case 0:
+            
                         #region Recatangle +50
                         
 
-                        double editedBeginningPointXPlus50 = coordinates[0].x+50;
+                        double editedBeginningPointXPlus50 = coordinates[0].x+200;
                         double editedbeginningPointY = coordinates[0].y;
 
-                        double editedEndPointXPlus50 = coordinates[0].x + 50;
+                        double editedEndPointXPlus50 = EndPointX + 200;
                         double editedEndPointY = coordinates[0].y;
 
                         string jsonPlus50 = string.Format(@"[
@@ -43,108 +39,43 @@ namespace GridPath.Helper
                 {{ ""x"": {2}, ""y"": {3} }},
                 {{ ""x"": {4}, ""y"": {5} }},
                 {{ ""x"": {6}, ""y"": {7} }}
-                ]", beginningPointX, beginningPointY, editedBeginningPointXPlus50, editedbeginningPointY, EndPointX, EndPointY, editedEndPointXPlus50, editedEndPointY);
+                ]",  editedBeginningPointXPlus50, editedbeginningPointY, beginningPointX, beginningPointY, EndPointX, EndPointY, editedEndPointXPlus50, EndPointY);
+                        CalculateRecatangleArea(EndPointX, editedEndPointXPlus50, EndPointY, beginningPointY);
                         jsonOfSideParcels.Add(jsonPlus50);
                         #endregion
 
                         #region Recatangle -50
+                        double editedBeginningPointXMinus50 = coordinates[0].x - 200;
 
-                        double secondEditedBeginningPointXMinus50 = coordinates[0].x - 50;
-                        double secondEditedbeginningPointY = coordinates[0].y;
-
-                        double secondEditedEndPointXMinus50 = coordinates[0].x - 50;
-                        double secondEditedEndPointY = coordinates[0].y;
+                        double editedEndPointXMinus50 = EndPointX - 200;
 
                         string jsonMinus50 = string.Format(@"[
                 {{ ""x"": {0}, ""y"": {1} }},
                 {{ ""x"": {2}, ""y"": {3} }},
                 {{ ""x"": {4}, ""y"": {5} }},
                 {{ ""x"": {6}, ""y"": {7} }}
-                ]", beginningPointX, beginningPointY, secondEditedBeginningPointXMinus50, secondEditedbeginningPointY, EndPointX, EndPointY, secondEditedEndPointXMinus50, secondEditedEndPointY);
+                ]", editedBeginningPointXMinus50, coordinates[0].y, beginningPointX, beginningPointY, EndPointX, EndPointY, editedEndPointXMinus50, EndPointY);
+                        CalculateRecatangleArea(EndPointX, editedEndPointXPlus50, EndPointY, beginningPointY);
                         jsonOfSideParcels.Add(jsonMinus50);
                         #endregion
 
 
-
-                        //TODO obdelniky od zakladniho Y do 1. stredoveho Y, X je stejně, popřipadně x+50, X-50
-
-                        break;
-                    case 1:
-                        double firstMiddleCoordinateY = coordinates[0].y + (coordinates[2].y - coordinates[0].y) / 2;
-                        double firstMiddleCoordinateX = coordinates[0].x;
-
-                        #region Recatangle +50(0->0.5)
-
-                        double firstMiddleCoordinateXPlus50 = beginningPointX + 50;
-                        double firstMiddleCoordinateYPlus50 = firstMiddleCoordinateY;
-
-                        double firstBeggingMiddleCoordinateXPlus50= beginningPointX + 50;
-                        double firstBeggingMiddleCoordinateYPlus50 = beginningPointY ;
-
-                        string jsonHalfPlus50MiddleCoordinateJson = string.Format(@"[
-                {{ ""x"": {0}, ""y"": {1} }},
-                {{ ""x"": {2}, ""y"": {3} }},
-                {{ ""x"": {4}, ""y"": {5} }},
-                {{ ""x"": {6}, ""y"": {7} }}
-                ]", beginningPointX, beginningPointY, firstBeggingMiddleCoordinateXPlus50, firstBeggingMiddleCoordinateYPlus50, firstMiddleCoordinateX, firstMiddleCoordinateY, firstMiddleCoordinateXPlus50, firstMiddleCoordinateYPlus50);
-                        jsonOfSideParcels.Add(jsonHalfPlus50MiddleCoordinateJson);
-
-                        #endregion
-
-                        #region Recatangle -50(0->0.5)
-
-                        double firstMiddleCoordinateXMinus50 = beginningPointX - 50;
-                        double firstMiddleCoordinateYMinus50 = firstMiddleCoordinateY;
-
-                        double firstBeggingMiddleCoordinateXMinus50 = beginningPointX - 50;
-                        double firstBeggingMiddleCoordinateYMinus50 = beginningPointY;
-
-                        string jsonHalfMinus50MiddleCoordinateJson = string.Format(@"[
-                {{ ""x"": {0}, ""y"": {1} }},
-                {{ ""x"": {2}, ""y"": {3} }},
-                {{ ""x"": {4}, ""y"": {5} }},
-                {{ ""x"": {6}, ""y"": {7} }}
-                ]", beginningPointX, beginningPointY, firstBeggingMiddleCoordinateXMinus50, firstBeggingMiddleCoordinateYMinus50, firstMiddleCoordinateX, firstMiddleCoordinateY, firstMiddleCoordinateXMinus50, firstMiddleCoordinateYMinus50);
-                        jsonOfSideParcels.Add(jsonHalfMinus50MiddleCoordinateJson);
-
-                        #endregion
-
-
-                        #region Recatangle +50(0.5->1)
-
-                        string jsonSecondHalfPlus50MiddleCoordinateJson = string.Format(@"[
-                {{ ""x"": {0}, ""y"": {1} }},
-                {{ ""x"": {2}, ""y"": {3} }},
-                {{ ""x"": {4}, ""y"": {5} }},
-                {{ ""x"": {6}, ""y"": {7} }}
-                ]", firstMiddleCoordinateX, firstMiddleCoordinateY, firstMiddleCoordinateXPlus50, firstMiddleCoordinateYPlus50, EndPointX, EndPointY, EndPointX+50, EndPointY);
-                        jsonOfSideParcels.Add(jsonSecondHalfPlus50MiddleCoordinateJson);
-
-                        #endregion
-
-
-                        #region Recatangle -50(0.5->1)
-
-
-                        string jsonSecondHalfMinus50MiddleCoordinateJson = string.Format(@"[
-                {{ ""x"": {0}, ""y"": {1} }},
-                {{ ""x"": {2}, ""y"": {3} }},
-                {{ ""x"": {4}, ""y"": {5} }},
-                {{ ""x"": {6}, ""y"": {7} }}
-                ]", firstMiddleCoordinateX, firstMiddleCoordinateY, firstMiddleCoordinateXMinus50, firstMiddleCoordinateYMinus50, EndPointX, EndPointY, EndPointX - 50, EndPointY);
-                        jsonOfSideParcels.Add(jsonSecondHalfMinus50MiddleCoordinateJson);
-
-                        #endregion
-
-                        break;
-                    default:
-                        break;
-                }
-                
-
-
-            }
             return jsonOfSideParcels;
+
+        }
+        public void CalculateRecatangleArea(double x4, double x3, double y4, double y1)
+        {
+            double plusx4 = Math.Abs(x4);
+            double plusx3 = Math.Abs(x3);
+            double plusy4 = Math.Abs(y4);
+            double plusy1 = Math.Abs(y1);
+                
+            double x    = Math.Abs(plusx4 - plusx3);
+            double y = Math.Abs(plusy4 - plusy1);
+            double obsah = x * y;
+            double pocetRozdeleni = Math.Ceiling((obsah / 5000));
+            Console.WriteLine($"Obsah obdelniku je {x * y}");
+            Console.WriteLine($"Pocet rozdeleni je {pocetRozdeleni}");
 
         }
         
